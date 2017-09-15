f6017cd15ce91388bc10df0da7e2f8e4cd26c29f
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-09-15 19:02:23 +0200

    Added safety net.

---
+++ src/test/java/com/gildedrose/NoRegressionsTest.java
@@ -0,0 +1,44 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NoRegressionsTest {
+
+    Item[] itemsBaseline = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
+
+    Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
+
+
+    @Test
+    public void noRegressions() {
+        GildedRoseBaseline baseline = new GildedRoseBaseline(itemsBaseline);
+        GildedRose gildedRose = new GildedRose(items);
+        baseline.updateQuality();
+        gildedRose.updateQuality();
+        for (int i = 0; i < itemsBaseline.length ; i++) {
+            assertEquals(itemsBaseline[i].quality, items[i].quality);
+        }
+    }
+}
