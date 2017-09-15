8c4d578af8966611ddff24044ba69877e0a05e17
xavi <xavi.hidalgo.fernandez@gmail.com>
2017-09-15 19:22:31 +0200

    WIP 001

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    private static String STATE1 = "Backstage passes to a TAFKAL80ETC concert";
+    private static String STATE_SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static String AGED_BRIE = "Aged Brie";
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -12,20 +15,20 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
 
             Item item = items[i];
+            String name = item.name;
 
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+            if (!name.equals(AGED_BRIE) && !name.equals(STATE1)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!name.equals(STATE_SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
             } else {
                 if (item.quality < 50) {
-
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (name.equals(STATE1)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -41,15 +44,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!name.equals(STATE_SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!name.equals(AGED_BRIE)) {
+                    if (!name.equals(STATE1)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!name.equals(STATE_SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
