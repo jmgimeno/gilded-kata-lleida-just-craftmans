f5f1ca8eb4810bdf5f10fb594337616f3ded5a57
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-09-16 12:04:02 +0200

    Simplifies code for Brie

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -106,52 +106,15 @@ class GildedRose {
         }
 
         protected void adjustQualityForExpiredItems() {
-            if (item.name.equals("Aged Brie")) {
-                incrementQuality();
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = item.quality - item.quality;
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.quality > 0) {
-                    return;
-                }
-                item.quality = item.quality - 1;
-            } else if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+            incrementQuality();
         }
 
         protected void updateSellIn() {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
         }
 
         protected void adjustQuality() {
-            if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11) {
-                        incrementQuality();
-                    }
-
-                    if (item.sellIn < 6) {
-                        incrementQuality();
-                    }
-                }
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.quality > 0) {
-                } else {
-                    item.quality = item.quality - 1;
-                }
-            } else if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+            incrementQuality();
         }
     }
 }
