07fbcd1c93b28ee1d559cce9a8c0406f333d5f8f
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-09-16 12:00:04 +0200

    Create subclass for Brie and copied methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,7 +8,10 @@ class GildedRose {
     }
 
     public static QualityUpdater clasify(Item item) {
-        return new QualityUpdater(item);
+        if (item.name.equals("Aged Brie"))
+            return new BrieUpdater(item);
+        else
+            return new QualityUpdater(item);
     }
 
     public void updateQuality() {
@@ -92,6 +95,62 @@ class GildedRose {
         protected void incrementQuality() {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
+            }
+        }
+    }
+
+
+    private static class BrieUpdater extends QualityUpdater {
+        public BrieUpdater(Item item) {
+            super(item);
+        }
+
+        protected void adjustQualityForExpiredItems() {
+            if (item.name.equals("Aged Brie")) {
+                incrementQuality();
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = item.quality - item.quality;
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
+                    return;
+                }
+                item.quality = item.quality - 1;
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
+
+        protected void updateSellIn() {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            } else {
+                item.sellIn = item.sellIn - 1;
+            }
+        }
+
+        protected void adjustQuality() {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
+                        incrementQuality();
+                    }
+
+                    if (item.sellIn < 6) {
+                        incrementQuality();
+                    }
+                }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
+                } else {
+                    item.quality = item.quality - 1;
+                }
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
