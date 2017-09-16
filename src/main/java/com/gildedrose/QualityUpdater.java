937bc8214f96d68cea06b9351225ecab49457771
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-09-16 11:44:26 +0200

    Decomposed updateQualityOfItem

--- src/main/java/com/gildedrose/QualityUpdater.java
+++ src/main/java/com/gildedrose/QualityUpdater.java
@@ -13,6 +13,38 @@ public class QualityUpdater {
     }
 
     void updateQualityOfItem() {
+        adjustQuality();
+
+        adjustSellIn();
+
+        if (item.sellIn < 0) {
+            reAdjustQuality();
+        }
+    }
+
+    private void reAdjustQuality() {
+        if (item.name.equals("Aged Brie")) {
+            incrementQuality();
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = item.quality - item.quality;
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0) {
+                return;
+            }
+            item.quality = item.quality - 1;
+        } else if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void adjustSellIn() {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void adjustQuality() {
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -36,26 +68,6 @@ public class QualityUpdater {
             }
         } else if (item.quality > 0) {
             item.quality = item.quality - 1;
-        }
-
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-                incrementQuality();
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = item.quality - item.quality;
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.quality > 0) {
-                    return;
-                }
-                item.quality = item.quality - 1;
-            } else if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
         }
     }
 
