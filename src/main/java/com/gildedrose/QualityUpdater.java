7c4cf39e187aaf709f758db5e65102e1e53a1028
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-09-16 10:52:05 +0200

    Merged ifs

--- src/main/java/com/gildedrose/QualityUpdater.java
+++ src/main/java/com/gildedrose/QualityUpdater.java
@@ -16,38 +16,26 @@ public class QualityUpdater {
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
+            }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        incrementQuality();
-                    }
+                if (item.sellIn < 11) {
+                    incrementQuality();
+                }
 
-                    if (item.sellIn < 6) {
-                        incrementQuality();
-                    }
+                if (item.sellIn < 6) {
+                    incrementQuality();
                 }
             }
-        } else {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                        if (item.sellIn < 11) {
-                            incrementQuality();
-                        }
-
-                        if (item.sellIn < 6) {
-                            incrementQuality();
-                        }
-                }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0) {
             } else {
-                if (item.quality > 0) {
-                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    } else {
-                        item.quality = item.quality - 1;
-                    }
-                }
+                item.quality = item.quality - 1;
             }
+        } else if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
 
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -58,17 +46,15 @@ public class QualityUpdater {
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
                 incrementQuality();
-            } else {
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = item.quality - item.quality;
-                } else {
-                    if (item.quality > 0) {
-                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            return;
-                        }
-                        item.quality = item.quality - 1;
-                    }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = item.quality - item.quality;
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
+                    return;
                 }
+                item.quality = item.quality - 1;
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
